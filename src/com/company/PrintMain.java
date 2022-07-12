8ad271a8dc7a95a120dc83d4839c49267addd82f
Alejo9806 <mello0616@gmail.com>
2022-07-12 12:39:20 -0500

    first commit

---
+++ src/com/company/PrintMain.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class PrintMain {
+    public static void main(String[] args) {
+
+        byte number1 = 1;
+        short number2 = 2; // 2 byte
+        int number3 = 3; // 4 byte
+        long number4 = 4; // 8 byte
+        char char1 = 'a';
+        float decimal = 64.21f;
+        double decimal2 = 12.21;
+        boolean check = true;
+        String name = "Alejandro";
+        Integer numberInteger = null;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(char1);
+        System.out.println(decimal);
+        System.out.println(decimal2);
+        System.out.println(check);
+        System.out.println(name);
+        System.out.println(numberInteger);
+    }
+}
